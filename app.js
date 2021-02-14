899292481f350ee88347badfe09eb22cfdd40691
MUHAMMAD940 <muhammadraj@gmail.com>
2021-02-14 04:40:56 -0800

    its my commit

--- app.js
+++ app.js
@@ -37,14 +37,23 @@ const getImages = (query) => {
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
-  let element = event.target;
-  element.classList.toggle("added");
-
-  let item = sliders.indexOf(img);
-  if (item === -1) {
-    sliders.push(img);
-  } 
+    let element = event.target;
+    element.classList.toggle('added');
+    
+    let item = sliders.indexOf(img);
+    if (item === -1) 
+    {
+        sliders.push(img);
+    }
+    else 
+    {
+        let splice = sliders.indexOf(img);
+        sliders.splice(splice, 1);
+    }
 };
+
+
+
 var timer
 const createSlider = () => {
   // check slider image length
